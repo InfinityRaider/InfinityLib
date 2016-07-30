bdbbc1b7ea64add8b8b249a197d9bdd2d878da33
InfinityRaider <the_xaf@hotmail.com>
2016-07-30 23:23:41 +0200

    Add get textures method for items

--- src/main/java/com/infinityraider/infinitylib/item/ICustomRenderedItem.java
+++ src/main/java/com/infinityraider/infinitylib/item/ICustomRenderedItem.java
@@ -3,8 +3,11 @@ package com.infinityraider.infinitylib.item;
 import com.infinityraider.infinitylib.render.item.IItemRenderingHandler;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+
+import java.util.List;
 
 public interface ICustomRenderedItem<I extends Item> {
     /**
@@ -13,6 +16,14 @@ public interface ICustomRenderedItem<I extends Item> {
      */
     @SideOnly(Side.CLIENT)
     IItemRenderingHandler<I> getRenderer();
+
+    /**
+     * Gets a list of all textures necessary to render this item,
+     * returned textures will be stitched to the atlas
+     * @return list of textures
+     */
+    @SideOnly(Side.CLIENT)
+    List<ResourceLocation> getTextures();
 
     /**
      * Gets an array of ResourceLocations used for the model of this block, all block states for this block will use this as key in the model registry
