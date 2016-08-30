82b6081f479e4948d31b652954bc723d8aefcc07
InfinityRaider <the_xaf@hotmail.com>
2016-08-30 21:32:32 +0200

    Remove unnecessary import

--- src/main/java/com/infinityraider/infinitylib/render/tessellation/TessellatorBakedQuad.java
+++ src/main/java/com/infinityraider/infinitylib/render/tessellation/TessellatorBakedQuad.java
@@ -12,7 +12,6 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * Helper class to construct vertices
