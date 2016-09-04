35945cfa3a36085b1a94b6cce906fecd21d70536
InfinityRaider <the_xaf@hotmail.com>
2016-09-04 18:04:20 +0200

    Extended the debugger with methods
    split method in a method for right clickign blocks, air and entities

--- src/main/java/com/infinityraider/infinitylib/utility/debug/DebugMode.java
+++ src/main/java/com/infinityraider/infinitylib/utility/debug/DebugMode.java
@@ -1,6 +1,7 @@
 package com.infinityraider.infinitylib.utility.debug;
 
 
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumFacing;
@@ -12,11 +13,11 @@ import net.minecraft.world.World;
  * Class to aid with debugging
  */
 public abstract class DebugMode {
-    /**
-     *
-     * @return
-     */
     public abstract String debugName();
 
-    public abstract void debugAction(ItemStack stack, EntityPlayer player, World world, BlockPos pos, EnumHand hand, EnumFacing side, float hitX, float hitY, float hitZ);
+    public abstract void debugActionBlockClicked(ItemStack stack, EntityPlayer player, World world, BlockPos pos, EnumHand hand, EnumFacing side, float hitX, float hitY, float hitZ);
+
+    public abstract void debugActionClicked(ItemStack stack, World world, EntityPlayer player, EnumHand hand);
+
+    public abstract void debugActionEntityClicked(ItemStack stack, EntityPlayer player, EntityLivingBase target, EnumHand hand);
 }
