6142278daeddbbbad010da3a5bea43e00d705c2d
InfinityRaider <the_xaf@hotmail.com>
2016-09-26 22:06:01 +0200

    Added IInventory -> IItemHandler and IItemHandler -> IInventory wrappers

---
+++ src/main/java/com/infinityraider/infinitylib/utility/inventory/InventoryItemHandler.java
@@ -0,0 +1,152 @@
+package com.infinityraider.infinitylib.utility.inventory;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextComponentTranslation;
+import net.minecraftforge.items.IItemHandler;
+
+import javax.annotation.Nullable;
+
+/**
+ * Simple class to wrap an IItemHandler as an IInventory while maintaining IItemHandler functionality
+ */
+public class InventoryItemHandler implements IInventoryItemHandler {
+    private final IItemHandler itemHandler;
+
+    public InventoryItemHandler(IItemHandler itemHandler) {
+        this.itemHandler = itemHandler;
+    }
+
+    public IItemHandler getItemHandler() {
+        return this.itemHandler;
+    }
+
+    /**
+     * --------------------
+     * IItemHandler methods
+     * --------------------
+     */
+
+    @Override
+    public int getSlots() {
+        return this.getItemHandler().getSlots();
+    }
+
+    @Nullable
+    @Override
+    public ItemStack getStackInSlot(int index) {
+        return this.getItemHandler().getStackInSlot(index);
+    }
+
+    @Override
+    public ItemStack insertItem(int slot, ItemStack stack, boolean simulate) {
+        return this.getItemHandler().insertItem(slot, stack, simulate);
+    }
+
+    @Override
+    public ItemStack extractItem(int slot, int amount, boolean simulate) {
+        return this.getItemHandler().extractItem(slot, amount, simulate);
+    }
+
+
+    /**
+     * ------------------
+     * IInventory methods
+     * ------------------
+     */
+
+    @Override
+    public int getSizeInventory() {
+        return this.getSlots();
+    }
+
+    @Nullable
+    @Override
+    public ItemStack decrStackSize(int index, int count) {
+        ItemStack stack = this.getStackInSlot(index);
+        if(stack != null) {
+            stack = this.extractItem(index, count, false);
+        }
+        return stack;
+    }
+
+    @Nullable
+    @Override
+    public ItemStack removeStackFromSlot(int index) {
+        ItemStack stack = this.getStackInSlot(index);
+        if(stack != null) {
+            stack = this.extractItem(index, stack.stackSize, false);
+        }
+        return stack;
+    }
+
+    @Override
+    public void setInventorySlotContents(int index, @Nullable ItemStack stack) {
+        ItemStack inSlot = this.getStackInSlot(index);
+        if(inSlot != null) {
+            this.extractItem(index, inSlot.stackSize, false);
+        }
+        this.insertItem(index, stack, false);
+    }
+
+    @Override
+    public int getInventoryStackLimit() {
+        return 64;
+    }
+
+    @Override
+    public void markDirty() {}
+
+    @Override
+    public boolean isUseableByPlayer(EntityPlayer player) {
+        return true;
+    }
+
+    @Override
+    public void openInventory(EntityPlayer player) {}
+
+    @Override
+    public void closeInventory(EntityPlayer player) {}
+
+    @Override
+    public boolean isItemValidForSlot(int index, ItemStack stack) {
+        ItemStack simulated = this.insertItem(index, stack, true);
+        return simulated.stackSize != stack.stackSize;
+    }
+
+    @Override
+    public int getField(int id) {
+        return 0;
+    }
+
+    @Override
+    public void setField(int id, int value) {}
+
+    @Override
+    public int getFieldCount() {
+        return 0;
+    }
+
+    @Override
+    public void clear() {
+        for(int i = 0; i < this.getSizeInventory(); i++) {
+            this.setInventorySlotContents(i, null);
+        }
+    }
+
+    @Override
+    public String getName() {
+        return "infinityLib.wrappedItemHandler";
+    }
+
+    @Override
+    public boolean hasCustomName() {
+        return false;
+    }
+
+    @Override
+    public ITextComponent getDisplayName() {
+        return new TextComponentTranslation(this.getName());
+    }
+}
---
+++ src/main/java/com/infinityraider/infinitylib/utility/inventory/ItemHandlerInventory.java
@@ -0,0 +1,116 @@
+package com.infinityraider.infinitylib.utility.inventory;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.text.ITextComponent;
+
+import javax.annotation.Nullable;
+
+/**
+ * Simple class to wrap an IInventory as an IItemHandler while maintaining IInventory functionality
+ */
+public class ItemHandlerInventory implements IInventoryItemHandler {
+    private final IInventory inventory;
+
+    public ItemHandlerInventory(IInventory inventory) {
+        this.inventory = inventory;
+    }
+
+    public IInventory getInventory() {
+        return inventory;
+    }
+
+    @Override
+    public int getSizeInventory() {
+        return this.getInventory().getSizeInventory();
+    }
+
+    @Nullable
+    @Override
+    public ItemStack getStackInSlot(int index) {
+        return  this.getInventory().getStackInSlot(index);
+    }
+
+    @Nullable
+    @Override
+    public ItemStack decrStackSize(int index, int count) {
+        return  this.getInventory().decrStackSize(index, count);
+    }
+
+    @Nullable
+    @Override
+    public ItemStack removeStackFromSlot(int index) {
+        return  this.getInventory().removeStackFromSlot(index);
+    }
+
+    @Override
+    public void setInventorySlotContents(int index, @Nullable ItemStack stack) {
+        this.getInventory().setInventorySlotContents(index, stack);
+    }
+
+    @Override
+    public int getInventoryStackLimit() {
+        return this.getInventory().getInventoryStackLimit();
+    }
+
+    @Override
+    public void markDirty() {
+        this.getInventory().markDirty();
+    }
+
+    @Override
+    public boolean isUseableByPlayer(EntityPlayer player) {
+        return this.getInventory().isUseableByPlayer(player);
+    }
+
+    @Override
+    public void openInventory(EntityPlayer player) {
+        this.getInventory().openInventory(player);
+    }
+
+    @Override
+    public void closeInventory(EntityPlayer player) {
+        this.getInventory().closeInventory(player);
+    }
+
+    @Override
+    public boolean isItemValidForSlot(int index, ItemStack stack) {
+        return this.getInventory().isItemValidForSlot(index, stack);
+    }
+
+    @Override
+    public int getField(int id) {
+        return this.getInventory().getField(id);
+    }
+
+    @Override
+    public void setField(int id, int value) {
+        this.getInventory().setField(id, value);
+    }
+
+    @Override
+    public int getFieldCount() {
+        return this.getInventory().getFieldCount();
+    }
+
+    @Override
+    public void clear() {
+        this.getInventory().clear();
+    }
+
+    @Override
+    public String getName() {
+        return this.getInventory().getName();
+    }
+
+    @Override
+    public boolean hasCustomName() {
+        return this.getInventory().hasCustomName();
+    }
+
+    @Override
+    public ITextComponent getDisplayName() {
+        return this.getInventory().getDisplayName();
+    }
+}
