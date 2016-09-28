d05ebff16b3a8943c872f409bbe5b45fd65ca827
InfinityRaider <the_xaf@hotmail.com>
2016-09-28 19:39:55 +0200

    Add TODO remark

--- src/main/java/com/infinityraider/infinitylib/item/IInfinityItem.java
+++ src/main/java/com/infinityraider/infinitylib/item/IInfinityItem.java
@@ -11,8 +11,8 @@ public interface IInfinityItem extends IToggleable {
 	String getInternalName();
 
 	List<String> getOreTags();
-	
+
+	//TODO: remove this, its not supposed to be here
 	@SideOnly(Side.CLIENT)
 	void registerItemRenderer();
-
 }
