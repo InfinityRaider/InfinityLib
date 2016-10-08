c111f36e46306a6b2e5d407f62e77643b979d6b7
InfinityRaider <the_xaf@hotmail.com>
2016-10-08 15:26:52 +0200

    Added default override for mergeItemStack() in ContainerBase

--- src/main/java/com/infinityraider/infinitylib/container/ContainerBase.java
+++ src/main/java/com/infinityraider/infinitylib/container/ContainerBase.java
@@ -4,6 +4,7 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.inventory.Container;
 import net.minecraft.inventory.Slot;
+import net.minecraft.item.ItemStack;
 
 public class ContainerBase extends Container {
     public static int PLAYER_INVENTORY_SIZE = 36;
@@ -26,5 +27,53 @@ public class ContainerBase extends Container {
     @Override
     public boolean canInteractWith(EntityPlayer player) {
         return true;
+    }
+
+    @Override
+    protected boolean mergeItemStack(ItemStack stack, int start, int stop, boolean backwards)  {
+        boolean foundSlot = false;
+        int slotIndex = backwards?stop-1:start;
+        Slot slot;
+        ItemStack stackInSlot;
+        //try to stack with existing stacks first
+        if(stack.isStackable()) {
+            while (stack.stackSize > 0 && (!backwards && slotIndex < stop || backwards && slotIndex >= start)) {
+                slot = this.inventorySlots.get(slotIndex);
+                stackInSlot = slot.getStack();
+                if(stackInSlot != null && slot.isItemValid(stack) && stackInSlot.getItem() == stack.getItem() && (!stack.getHasSubtypes() || stack.getItemDamage() == stackInSlot.getItemDamage()) && ItemStack.areItemStackTagsEqual(stack, stackInSlot)) {
+                    int combinedSize = stackInSlot.stackSize + stack.stackSize;
+                    if(combinedSize <= stack.getMaxStackSize()) {
+                        stack.stackSize = 0;
+                        stackInSlot.stackSize = combinedSize;
+                        slot.onSlotChanged();
+                        foundSlot = true;
+                    }
+                    else if(stackInSlot.stackSize < stack.getMaxStackSize()) {
+                        stack.stackSize -= stack.getMaxStackSize() - stackInSlot.stackSize;
+                        stackInSlot.stackSize = stack.getMaxStackSize();
+                        slot.onSlotChanged();
+                        foundSlot = true;
+                    }
+                }
+                slotIndex = backwards?slotIndex-1:slotIndex+1;
+            }
+        }
+        //put in empty slot
+        if(stack.stackSize > 0) {
+            slotIndex = backwards?stop-1:start;
+            while(!backwards && slotIndex < stop || backwards && slotIndex >= start && !foundSlot) {
+                slot = this.inventorySlots.get(slotIndex);
+                stackInSlot = slot.getStack();
+                if(stackInSlot == null && slot.isItemValid(stack)) {
+                    slot.putStack(stack.copy());
+                    slot.onSlotChanged();
+                    stack.stackSize = 0;
+                    foundSlot = true;
+                    break;
+                }
+                slotIndex = backwards?slotIndex-1:slotIndex+1;
+            }
+        }
+        return foundSlot;
     }
 }
