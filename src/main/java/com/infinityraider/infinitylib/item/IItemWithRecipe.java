f170fa3e2601f38ef0fbcac2c6770e2747cf309e
InfinityRaider <the_xaf@hotmail.com>
2016-10-08 17:22:20 +0200

    IItemWithRecipe no longer extedns IInfinityItem
    (becasue it also affects blocks)

--- src/main/java/com/infinityraider/infinitylib/item/IItemWithRecipe.java
+++ src/main/java/com/infinityraider/infinitylib/item/IItemWithRecipe.java
@@ -7,7 +7,7 @@ import java.util.List;
 /**
  * Interface used to ease recipe registering
  */
-public interface IItemWithRecipe extends IInfinityItem {
+public interface IItemWithRecipe {
     /**
      * @return a list of all recipes to be registered
      */
