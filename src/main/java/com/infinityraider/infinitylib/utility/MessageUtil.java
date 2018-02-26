1c3fb3980e3fa39f7dce52bb7d735906e0d58742
RlonRyan <RlonRyan@users.noreply.github.com>
2018-02-25 20:04:25 -0700

    Tweak MessageUtil
    This is so that the game doesn't crash when you accidentally try to message a null player (i.e. you thought it was a real player but it turned out to be some automated construct).

--- src/main/java/com/infinityraider/infinitylib/utility/MessageUtil.java
+++ src/main/java/com/infinityraider/infinitylib/utility/MessageUtil.java
@@ -3,6 +3,8 @@
 package com.infinityraider.infinitylib.utility;
 
 import java.text.MessageFormat;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.text.TextComponentString;
 
@@ -11,13 +13,23 @@ import net.minecraft.util.text.TextComponentString;
  */
 public class MessageUtil {
 
-    public static final void messagePlayer(EntityPlayer player, String format, Object... args) {
+    public static final void messagePlayer(@Nullable EntityPlayer player, @Nonnull String format, Object... args) {
+        // If the player is null don't do anything.
+        if (player == null) {
+            return;
+        }
+        
+        // Holder for formatted message.
         String message;
+        
+        // Format the message.
         try {
             message = MessageFormat.format(format, args);
         } catch (IllegalArgumentException ex) {
             message = "Message Formatting Error: \"" + format + "\"!";
         }
+        
+        // Send the message.
         player.sendMessage(new TextComponentString(message));
     }
 
