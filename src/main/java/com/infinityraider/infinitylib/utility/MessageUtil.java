04dda60a68480b4746d0f71cf76f3178f7de0605
RlonRyan <RlonRyan@users.noreply.github.com>
2018-02-27 10:59:14 -0700

    Tiny Tweak to MessageUtil
    This tiny tweak allows for the use of Minecraft 1-character format codes prefixed with the '`' symbol, for nicer message formatting.

--- src/main/java/com/infinityraider/infinitylib/utility/MessageUtil.java
+++ src/main/java/com/infinityraider/infinitylib/utility/MessageUtil.java
@@ -12,23 +12,26 @@ import net.minecraft.util.text.TextComponentString;
  * Utility class for messaging players.
  */
 public class MessageUtil {
+    
+    public static final char COLOR_CODE_ESCAPE = '`';
+    public static final char COLOR_CODE_REPLACEMENT = '\u00a7';
 
     public static final void messagePlayer(@Nullable EntityPlayer player, @Nonnull String format, Object... args) {
         // If the player is null don't do anything.
         if (player == null) {
             return;
         }
-        
+
         // Holder for formatted message.
         String message;
-        
+
         // Format the message.
         try {
-            message = MessageFormat.format(format, args);
+            message = MessageFormat.format(format, args).replace(COLOR_CODE_ESCAPE, COLOR_CODE_REPLACEMENT);
         } catch (IllegalArgumentException ex) {
             message = "Message Formatting Error: \"" + format + "\"!";
         }
-        
+
         // Send the message.
         player.sendMessage(new TextComponentString(message));
     }
