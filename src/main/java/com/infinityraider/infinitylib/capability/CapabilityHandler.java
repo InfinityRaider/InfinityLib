7db6cc54b7b54532a17a1a2a7996f8b2a99b7153
InfinityRaider <theoneandonlyraider@gmail.com>
2020-12-06 19:54:50 +0000

    Make registering of Capabilities threadsafe

--- src/main/java/com/infinityraider/infinitylib/capability/CapabilityHandler.java
+++ src/main/java/com/infinityraider/infinitylib/capability/CapabilityHandler.java
@@ -1,5 +1,6 @@
 package com.infinityraider.infinitylib.capability;
 
+import com.google.common.collect.Sets;
 import com.infinityraider.infinitylib.InfinityLib;
 import com.infinityraider.infinitylib.utility.ISerializable;
 import net.minecraft.entity.player.PlayerEntity;
@@ -18,20 +19,17 @@ public class CapabilityHandler {
         return INSTANCE;
     }
 
-    private final Map<Class<? extends ICapabilityProvider>, List<ICapabilityImplementation<ICapabilityProvider, ? extends ISerializable>>> map;
+    private final Set<ICapabilityImplementation<ICapabilityProvider, ? extends ISerializable>> capabilityImplementations;
 
     private CapabilityHandler() {
-        this.map = new HashMap<>();
+        this.capabilityImplementations = Sets.newConcurrentHashSet();
         InfinityLib.instance.registerEventHandler(this);
     }
 
     @SuppressWarnings("unchecked")
-    public void registerCapability(ICapabilityImplementation implementation) {
-        if(!this.map.containsKey(implementation.getCarrierClass())) {
-            this.map.put(implementation.getCarrierClass(), new ArrayList<>());
-        }
-        this.map.get(implementation.getCarrierClass()).add(implementation);
-        CapabilityManager.INSTANCE.register(implementation.getCapabilityClass(), new CapabilityStorage(), () -> null);
+    public void registerCapability(ICapabilityImplementation<ICapabilityProvider, ? extends ISerializable> implementation) {
+        this.capabilityImplementations.add(implementation);
+        CapabilityManager.INSTANCE.register(implementation.getCapabilityClass(), new CapabilityStorage<>(), () -> null);
     }
 
     protected <T extends ICapabilityProvider, C extends ISerializable> void addCapability(
@@ -45,11 +43,10 @@ public class CapabilityHandler {
         Object object = event.getObject();
         T carrier = (T) object;
         Class<T> clazz = (Class<T>) carrier.getClass();
-        this.map.entrySet().stream().filter(entry -> entry.getKey().isAssignableFrom(clazz)).forEach(
-                entry -> entry.getValue().stream()
-                        .filter(impl -> impl.getCarrierClass().isAssignableFrom(clazz))
-                        .filter(impl -> impl.shouldApplyCapability(carrier))
-                        .forEach(impl -> this.addCapability(event, impl, carrier)));
+        this.capabilityImplementations.stream()
+                .filter(impl -> impl.getCarrierClass().isAssignableFrom(clazz))
+                .filter(impl -> impl.shouldApplyCapability(carrier))
+                .forEach(impl -> this.addCapability(event, impl, carrier));
     }
 
     @SubscribeEvent
@@ -66,11 +63,7 @@ public class CapabilityHandler {
         PlayerEntity oldPlayer = event.getOriginal();
         PlayerEntity newPlayer = event.getPlayer();
         if(oldPlayer != null && newPlayer != null && !oldPlayer.getEntityWorld().isRemote) {
-            List<ICapabilityImplementation<ICapabilityProvider, ? extends ISerializable>> list = this.map.get(oldPlayer.getClass());
-            if(list == null) {
-                return;
-            }
-            list.stream()
+            this.capabilityImplementations.stream()
                     .filter(impl -> PlayerEntity.class.isAssignableFrom(impl.getCarrierClass()))
                     .forEach(impl -> {
                         oldPlayer.getCapability(impl.getCapability(), null).ifPresent(oldProps -> {
