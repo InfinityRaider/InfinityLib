7925007dbfde6f71a5cffc89be55b1eaf9322089
InfinityRaider <theoneandonlyraider@gmail.com>
2020-12-07 16:35:21 +0000

    Remove redundant Effect rendering
    Vanilla handles it fine already

--- src/main/java/com/infinityraider/infinitylib/effect/EffectBase.java
+++ src/main/java/com/infinityraider/infinitylib/effect/EffectBase.java
@@ -1,9 +1,6 @@
 package com.infinityraider.infinitylib.effect;
 
-import com.mojang.blaze3d.matrix.MatrixStack;
-import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.AbstractGui;
 import net.minecraft.client.renderer.texture.PotionSpriteUploader;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.potion.Effect;
@@ -36,13 +33,5 @@ public class EffectBase extends Effect implements IInfinityEffect {
     protected TextureAtlasSprite getSprite() {
         PotionSpriteUploader potionspriteuploader = Minecraft.getInstance().getPotionSpriteUploader();
         return potionspriteuploader.getSprite(this);
-    }
-
-    @OnlyIn(Dist.CLIENT)
-    protected void drawInGui(MatrixStack transforms, int x, int y, AbstractGui gui, int width, int height, float alpha) {
-        RenderSystem.color4f(1.0F, 1.0F, 1.0F, alpha);
-        TextureAtlasSprite sprite = this.getSprite();
-        Minecraft.getInstance().getTextureManager().bindTexture(sprite.getAtlasTexture().getTextureLocation());
-        AbstractGui.blit(transforms, x, y, gui.getBlitOffset(), width, height, this.getSprite());
     }
 }
