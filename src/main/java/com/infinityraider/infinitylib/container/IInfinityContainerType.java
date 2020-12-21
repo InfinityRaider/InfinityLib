b19d2e380bae9d38edbe037fe5ba72f623ac33f7
InfinityRaider <theoneandonlyraider@gmail.com>
2020-12-21 11:38:28 +0000

    Fix a generics bug

--- src/main/java/com/infinityraider/infinitylib/container/IInfinityContainerType.java
+++ src/main/java/com/infinityraider/infinitylib/container/IInfinityContainerType.java
@@ -4,10 +4,8 @@ import com.infinityraider.infinitylib.utility.IInfinityRegistrable;
 import net.minecraft.client.gui.IHasContainer;
 import net.minecraft.client.gui.ScreenManager;
 import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.inventory.container.Container;
 import net.minecraft.inventory.container.ContainerType;
-import net.minecraft.util.text.ITextComponent;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
@@ -20,12 +18,12 @@ public interface IInfinityContainerType extends IInfinityRegistrable<ContainerTy
 
     interface IGuiFactory<T extends Container> {
         @OnlyIn(Dist.CLIENT)
-        <U extends Screen & IHasContainer<T>> U createGui(T container, PlayerInventory inventory, ITextComponent name);
+        <U extends Screen & IHasContainer<T>> ScreenManager.IScreenFactory<T, U> getGuiScreenProvider();
     }
 
     @OnlyIn(Dist.CLIENT)
     @SuppressWarnings("unchecked")
     static <T extends Container, U extends Screen & IHasContainer<T>> ScreenManager.IScreenFactory<T,U> castGuiFactory(IGuiFactory factory) {
-        return factory::createGui;
+        return factory.getGuiScreenProvider();
     }
 }
