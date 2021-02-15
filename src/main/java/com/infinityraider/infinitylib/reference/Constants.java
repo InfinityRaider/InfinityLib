184880110203a6a1ead7372b04e19bf4e79803ef
InfinityRaider <theoneandonlyraider@gmail.com>
2021-02-15 11:21:12 +0000

    Add some more useful constants

--- src/main/java/com/infinityraider/infinitylib/reference/Constants.java
+++ src/main/java/com/infinityraider/infinitylib/reference/Constants.java
@@ -11,4 +11,37 @@ public class Constants {
      * back on calculations.
      */
     public static final float UNIT = 1.0f / WHOLE;
+
+    /**
+     * The value of half a block. Pre-calculated as to cut
+     * back on calculations.
+     */
+    public static final float HALF = UNIT * WHOLE / 2;
+
+    /**
+     * The value of quarter a block. Pre-calculated as to cut
+     * back on calculations.
+     */
+    public static final float QUARTER = UNIT * WHOLE / 4;
+
+    /**
+     * The value of three quarter a block. Pre-calculated as to cut
+     * back on calculations.
+     */
+    public static final float THREE_QUARTER = 3 * QUARTER;
+
+    /**
+     * The representation of 1 bucket(b) in millibuckets(mB).
+     */
+    int BUCKET_mB = 1000;
+
+    /**
+     * The representation of 1/2 a bucket(b) in millibuckets(mB).
+     */
+    int HALF_BUCKET_mB = BUCKET_mB / 2;
+
+    /**
+     * The representation of 1/4 a bucket(b) in millibuckets(mB).
+     */
+    int QUARTER_BUCKET_mB = BUCKET_mB / 4;
 }
