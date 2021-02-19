c7ceb292636d33c96d42d7210290685da37d447e
InfinityRaider <theoneandonlyraider@gmail.com>
2021-02-19 10:07:53 +0000

    Version bump

--- src/main/java/com/infinityraider/infinitylib/reference/Reference.java
+++ src/main/java/com/infinityraider/infinitylib/reference/Reference.java
@@ -5,11 +5,11 @@ public final class Reference {
     public static final String MOD_NAME = /*^${mod.name}^*/ "InfinityLib";
     public static final String AUTHOR = /*^${mod.author}^*/ "InfinityRaider";
 
-    public static final String VER_MAJOR = /*^${mod.version_major}^*/ "0";
+    public static final String VER_MAJOR = /*^${mod.version_major}^*/ "2";
     public static final String VER_MINOR = /*^${mod.version_minor}^*/ "0";
-    public static final String VER_PATCH = /*^${mod.version_patch}^*/ "0";
-    public static final String MOD_VERSION = /*^${mod.version}^*/ "0.0.0";
-    public static final String VERSION = /*^${mod.version_minecraft}-${mod.version}^*/ "0.0-0.0.0";
+    public static final String VER_PATCH = /*^${mod.version_patch}^*/ "1";
+    public static final String MOD_VERSION = /*^${mod.version}^*/ VER_MAJOR + "." + VER_MINOR + "." + VER_PATCH;
+    public static final String VERSION = /*^${mod.version_minecraft}-${mod.version}^*/ "1.16.4-" + MOD_VERSION;
 
     public static final String GUI_FACTORY_CLASS = "com.infinityraider.infinitylib.gui.GuiFactory";
 
