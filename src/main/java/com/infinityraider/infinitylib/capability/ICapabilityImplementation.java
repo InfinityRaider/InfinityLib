19d4c08d0f7d71a8ee9a4d05228051e70d8c962d
InfinityRaider <theoneandonlyraider@gmail.com>
2021-03-23 12:51:22 +0000

    Add default Capability getters to CapabilityImplementation

--- src/main/java/com/infinityraider/infinitylib/capability/ICapabilityImplementation.java
+++ src/main/java/com/infinityraider/infinitylib/capability/ICapabilityImplementation.java
@@ -1,8 +1,12 @@
 package com.infinityraider.infinitylib.capability;
 
+import net.minecraft.util.Direction;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.capabilities.ICapabilityProvider;
+import net.minecraftforge.common.util.LazyOptional;
+
+import javax.annotation.Nullable;
 
 public interface ICapabilityImplementation<C extends ICapabilityProvider, V> {
     Capability<V> getCapability();
@@ -14,4 +18,12 @@ public interface ICapabilityImplementation<C extends ICapabilityProvider, V> {
     ResourceLocation getCapabilityKey();
 
     Class<C> getCarrierClass();
+
+    default LazyOptional<V> getCapability(C carrier) {
+        return carrier.getCapability(this.getCapability());
+    }
+
+    default LazyOptional<V> getCapability(C carrier, @Nullable Direction dir) {
+        return carrier.getCapability(this.getCapability(), dir);
+    }
 }
