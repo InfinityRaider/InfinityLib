2940232fdda1e718dedef95e2d9545666c2de81b
InfinityRaider <theoneandonlyraider@gmail.com>
2022-04-24 16:12:35 +0100

    TagUtil cleanup

--- src/main/java/com/infinityraider/infinitylib/utility/TagUtil.java
+++ src/main/java/com/infinityraider/infinitylib/utility/TagUtil.java
@@ -7,6 +7,7 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
@@ -19,9 +20,11 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.ItemLike;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.StateHolder;
+import net.minecraft.world.level.block.state.properties.Property;
 import net.minecraft.world.level.material.Fluid;
 import net.minecraft.world.level.material.FluidState;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -59,10 +62,10 @@ public final class TagUtil {
     }
 
     /**
-     * Determines if the given string represents a valid oredict resource entry.
+     * Determines if the given string represents a valid tag entry.
      *
-     * @param prefix
-     * @param suffix
+     * @param prefix the prefix
+     * @param suffix the suffix
      * @return {@literal true} if and only if the given string represents a valid oredict entry, {@literal false} otherwise.
      */
     public static <T extends IForgeRegistryEntry<T>> boolean isValidTag(@Nonnull ITagManager<T> registry, @Nonnull String prefix, @Nonnull String suffix) {
@@ -76,14 +79,6 @@ public final class TagUtil {
             return false;
         } else {
             return true;
-        }
-    }
-
-    private static Optional<ItemStack> createStackFromObject(Object object, int amount) {
-        if(object instanceof ItemLike) {
-            return Optional.of(new ItemStack((ItemLike) object, amount));
-        } else {
-            return Optional.empty();
         }
     }
 
@@ -207,13 +202,25 @@ public final class TagUtil {
      *
      * @param element the registry name of the Block.
      * @param useTags to check if the element corresponds to a registry name in the tag registry instead
-     * @param data a json string representation of the required BlockState Properties and their value
-     * @param ignoredData a List of BlockState Properties to ignore
      * @return a Collection containing all matching BlockStates, or empty
      * optional.
      */
     @Nonnull
-    public static final Collection<BlockState> fetchBlockStates(@Nullable String element, boolean useTags, String data, List<String> ignoredData) {
+    public static Collection<BlockState> fetchBlockStates(@Nullable String element, boolean useTags) {
+        return fetchBlockStates(element, useTags, Collections.emptyList());
+    }
+
+    /**
+     * Fetches a Collection of all BlockStates respecting the given attributes, if possible.
+     *
+     * @param element the registry name of the Block.
+     * @param useTags to check if the element corresponds to a registry name in the tag registry instead
+     * @param blockStateData a list of strings representing block state property values
+     * @return a Collection containing all matching BlockStates, or empty
+     * optional.
+     */
+    @Nonnull
+    public static Collection<BlockState> fetchBlockStates(@Nullable String element, boolean useTags, List<String> blockStateData) {
         // If null or empty return nothing.
         if (element == null || element.isEmpty()) {
             return Collections.emptyList();
@@ -224,34 +231,33 @@ public final class TagUtil {
 
         // If only 1 part, then assume is suffix.
         if (parts.length == 1) {
-            return fetchBlockStates("minecraft", parts[0], useTags, data, ignoredData);
+            return fetchBlockStates("minecraft", parts[0], useTags, blockStateData);
         } else if (parts.length == 2) {
-            return fetchBlockStates(parts[0], parts[1], useTags, data, ignoredData);
+            return fetchBlockStates(parts[0], parts[1], useTags, blockStateData);
         } else if (parts.length > 2) {
-            InfinityLib.instance.getLogger().warn("Invalid stack identifier detected!\n\tGiven: \"{0}\"\n\tAssuming: \"{1}:{2}\"", element, parts[0], parts[1]);
-            return fetchBlockStates(parts[0], parts[1], useTags, data, ignoredData);
+            InfinityLib.instance.getLogger().warn("Invalid block identifier detected!\n\tGiven: \"{0}\"\n\tAssuming: \"{1}:{2}\"", element, parts[0], parts[1]);
+            return fetchBlockStates(parts[0], parts[1], useTags, blockStateData);
         } else {
             throw new AssertionError("String.split() method worked incorrectly. This should be an impossible error.");
         }
     }
 
     @Nonnull
-    public static final Collection<BlockState> fetchBlockStates(@Nonnull String prefix, @Nonnull String suffix,
-                                                                boolean useTags, @Nullable String data, List<String> ignoredData) {
+    public static Collection<BlockState> fetchBlockStates(@Nonnull String prefix, @Nonnull String suffix, boolean useTags, List<String> blockStateData) {
         // Validate
         Preconditions.checkNotNull(prefix, "A Block identifier must have a non-null prefix!");
         Preconditions.checkNotNull(suffix, "A Block identifier must have a non-null suffix!");
 
         // Test if the prefix is the special ore dict prefix.
         if (useTags) {
-            return fetchBlockStatesFromTag(prefix, suffix, data, ignoredData);
+            return fetchBlockStatesFromTag(prefix, suffix, blockStateData);
         } else {
-            return fetchBlockStatesNormal(prefix, suffix, data, ignoredData);
+            return fetchBlockStatesNormal(prefix, suffix, blockStateData);
         }
     }
 
     @Nonnull
-    private static Collection<BlockState> fetchBlockStatesNormal(@Nonnull String prefix, @Nonnull String suffix,  @Nullable String data, List<String> ignoredData) {
+    private static Collection<BlockState> fetchBlockStatesNormal(@Nonnull String prefix, @Nonnull String suffix,  List<String> blockStateData) {
         // Step 0. Fetch the item.
         final Block block = ForgeRegistries.BLOCKS.getValue(new ResourceLocation(prefix, suffix));
 
@@ -262,17 +268,24 @@ public final class TagUtil {
         }
 
         // Step 2. Return the block
-        return block.getStateDefinition().getPossibleStates();  // TODO: filter based on data and ignoredData
+        return block.getStateDefinition().getPossibleStates()
+                .stream()
+                .filter(state -> stateMatchesData(state, blockStateData))
+                .collect(Collectors.toList());
     }
 
     @Nonnull
-    private static Collection<BlockState> fetchBlockStatesFromTag(@Nonnull String prefix, @Nonnull String suffix, @Nullable String data, List<String> ignoredData) {
+    private static Collection<BlockState> fetchBlockStatesFromTag(@Nonnull String prefix, @Nonnull String suffix, List<String> blockStateData) {
         // Do the thing.
         ITagManager<Block> registry = ForgeRegistries.BLOCKS.tags();
+        if(registry == null) {
+            InfinityLib.instance.getLogger().error("Block tag manager is null, could not fetch tag " + prefix + ":" + suffix);
+            return Collections.emptyList();
+        }
         TagKey<Block> key = registry.createTagKey(new ResourceLocation(prefix, suffix));
         return registry.getTag(key).stream()
                 .flatMap(block -> block.getStateDefinition().getPossibleStates().stream())
-                .filter(state -> true)  // TODO: filter based on data and ignoredData
+                .filter(state -> stateMatchesData(state, blockStateData))
                 .collect(Collectors.toList());
     }
 
@@ -281,13 +294,25 @@ public final class TagUtil {
      *
      * @param element the registry name of the Fluid.
      * @param useTags to check if the element corresponds to a registry name in the tag registry instead
-     * @param data a json string representation of the required BlockState Properties and their value
-     * @param ignoredData a List of BlockState Properties to ignore
-     * @return a Collection containing all matching BlockStates, or empty
+     * @return a Collection containing all matching FluidStates, or empty
      * optional.
      */
     @Nonnull
-    public static final Collection<FluidState> fetchFluidStates(@Nullable String element, boolean useTags, String data, List<String> ignoredData) {
+    public static Collection<FluidState> fetchFluidStates(@Nullable String element, boolean useTags) {
+        return fetchFluidStates(element, useTags, Collections.emptyList());
+    }
+
+    /**
+     * Fetches a Collection of all FluidStates respecting the given attributes, if possible.
+     *
+     * @param element the registry name of the Fluid.
+     * @param useTags to check if the element corresponds to a registry name in the tag registry instead
+     * @param fluidStateData a List of BlockState Properties to ignore
+     * @return a Collection containing all matching FluidStates, or empty
+     * optional.
+     */
+    @Nonnull
+    public static Collection<FluidState> fetchFluidStates(@Nullable String element, boolean useTags, List<String> fluidStateData) {
         // If null or empty return nothing.
         if (element == null || element.isEmpty()) {
             return Collections.emptyList();
@@ -298,34 +323,33 @@ public final class TagUtil {
 
         // If only 1 part, then assume is suffix.
         if (parts.length == 1) {
-            return fetchFluidStates("minecraft", parts[0], useTags, data, ignoredData);
+            return fetchFluidStates("minecraft", parts[0], useTags, fluidStateData);
         } else if (parts.length == 2) {
-            return fetchFluidStates(parts[0], parts[1], useTags, data, ignoredData);
+            return fetchFluidStates(parts[0], parts[1], useTags, fluidStateData);
         } else if (parts.length > 2) {
             InfinityLib.instance.getLogger().warn("Invalid stack identifier detected!\n\tGiven: \"{0}\"\n\tAssuming: \"{1}:{2}\"", element, parts[0], parts[1]);
-            return fetchFluidStates(parts[0], parts[1], useTags, data, ignoredData);
+            return fetchFluidStates(parts[0], parts[1], useTags, fluidStateData);
         } else {
             throw new AssertionError("String.split() method worked incorrectly. This should be an impossible error.");
         }
     }
 
     @Nonnull
-    public static final Collection<FluidState> fetchFluidStates(@Nonnull String prefix, @Nonnull String suffix,
-                                                                boolean useTags, @Nullable String data, List<String> ignoredData) {
+    public static Collection<FluidState> fetchFluidStates(@Nonnull String prefix, @Nonnull String suffix, boolean useTags, List<String> fluidStateData) {
         // Validate
         Preconditions.checkNotNull(prefix, "A Fluid identifier must have a non-null prefix!");
         Preconditions.checkNotNull(suffix, "A Fluid identifier must have a non-null suffix!");
 
         // Test if the prefix is the special ore dict prefix.
         if (useTags) {
-            return fetchFluidStatesFromTag(prefix, suffix, data, ignoredData);
+            return fetchFluidStatesFromTag(prefix, suffix, fluidStateData);
         } else {
-            return fetchFluidStatesNormal(prefix, suffix, data, ignoredData);
+            return fetchFluidStatesNormal(prefix, suffix, fluidStateData);
         }
     }
 
     @Nonnull
-    private static Collection<FluidState> fetchFluidStatesNormal(@Nonnull String prefix, @Nonnull String suffix,  @Nullable String data, List<String> ignoredData) {
+    private static Collection<FluidState> fetchFluidStatesNormal(@Nonnull String prefix, @Nonnull String suffix, List<String> fluidStateData) {
         // Step 0. Fetch the item.
         final Fluid fluid = ForgeRegistries.FLUIDS.getValue(new ResourceLocation(prefix, suffix));
 
@@ -336,18 +360,55 @@ public final class TagUtil {
         }
 
         // Step 2. Return the Fluid States
-        return fluid.getStateDefinition().getPossibleStates();  // TODO: filter based on data and ignoredData
+        return fluid.getStateDefinition().getPossibleStates()
+                .stream()
+                .filter(state -> stateMatchesData(state, fluidStateData))
+                .collect(Collectors.toList());
     }
 
     @Nonnull
-    private static Collection<FluidState> fetchFluidStatesFromTag(@Nonnull String prefix, @Nonnull String suffix, @Nullable String data, List<String> ignoredData) {
+    private static Collection<FluidState> fetchFluidStatesFromTag(@Nonnull String prefix, @Nonnull String suffix, List<String> fluidStateData) {
         // Do the thing.
         ITagManager<Fluid> registry = ForgeRegistries.FLUIDS.tags();
+        if(registry == null) {
+            InfinityLib.instance.getLogger().error("Fluid tag manager is null, could not fetch tag " + prefix + ":" + suffix);
+            return Collections.emptyList();
+        }
         TagKey<Fluid> key = registry.createTagKey(new ResourceLocation(prefix, suffix));
         return registry.getTag(key).stream()
                 .flatMap(fluid -> fluid.getStateDefinition().getPossibleStates().stream())
-                .filter(state -> true)  // TODO: filter based on data and ignoredData
+                .filter(state -> stateMatchesData(state, fluidStateData))
                 .collect(Collectors.toList());
+    }
+
+    private static boolean stateMatchesData(BlockState state, List<String> blockStateData) {
+        return stateMatchesData(state, blockStateData, BlockState::getBlock, Block::getStateDefinition);
+    }
+
+    private static boolean stateMatchesData(FluidState state, List<String> fluidStateData) {
+        return stateMatchesData(state, fluidStateData, FluidState::getType, Fluid::getStateDefinition);
+    }
+
+    public static <S extends StateHolder<O, S>, O extends IForgeRegistryEntry<O>> boolean stateMatchesData( S state, List<String> data, Function<S, O> ownerFunc, Function<O, StateDefinition<O, S>> defFunc) {
+        O owner = ownerFunc.apply(state);
+        return data.stream().allMatch(entry -> {
+            // Split string in property and value
+            String[] split = entry.split("=");
+            if (split.length < 2) {
+                InfinityLib.instance.getLogger().error("Invalid state property definition: " + entry);
+                return false;
+            }
+            StateDefinition<O, S> definition = defFunc.apply(owner);
+            Property<?> prop = definition.getProperty(split[0]);
+            if (prop == null) {
+                InfinityLib.instance.getLogger().error("Property " + split[0] + " does not exist on " + owner.getRegistryName());
+                return false;
+            }
+            return prop.getValue(split[1]).map(value -> state.getValue(prop).equals(value)).orElseGet(() -> {
+                InfinityLib.instance.getLogger().error("Value " + split[1] + " does not exist for property " + split[0] + " on " + owner.getRegistryName());
+                return false;
+            });
+        });
     }
 
     /**
