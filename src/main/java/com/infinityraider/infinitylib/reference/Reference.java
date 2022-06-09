e6eb9d59c9bb2c08bfb779406ddb0d3277838eb3
InfinityRaider <theoneandonlyraider@gmail.com>
2022-06-09 22:59:13 +0100

    Version bump

--- src/main/java/com/infinityraider/infinitylib/reference/Reference.java
+++ src/main/java/com/infinityraider/infinitylib/reference/Reference.java
@@ -7,7 +7,7 @@ public final class Reference {
 
     public static final String VER_MAJOR = /*^${mod.version_major}^*/ "2";
     public static final String VER_MINOR = /*^${mod.version_minor}^*/ "1";
-    public static final String VER_PATCH = /*^${mod.version_patch}^*/ "0";
+    public static final String VER_PATCH = /*^${mod.version_patch}^*/ "2";
     public static final String MOD_VERSION = /*^${mod.version}^*/ VER_MAJOR + "." + VER_MINOR + "." + VER_PATCH;
     public static final String VERSION = /*^${mod.version_minecraft}-${mod.version}^*/ "1.18.2-" + MOD_VERSION;
 
